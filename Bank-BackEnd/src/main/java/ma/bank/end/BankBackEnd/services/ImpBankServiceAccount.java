70b6c0dd69fbd58afc5456b895d21bb3e7c5067c
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2024-01-20 00:42:13 +0100

    Inject Repositories

--- Bank-BackEnd/src/main/java/ma/bank/end/BankBackEnd/services/ImpBankServiceAccount.java
+++ Bank-BackEnd/src/main/java/ma/bank/end/BankBackEnd/services/ImpBankServiceAccount.java
@@ -1,16 +1,32 @@
 package ma.bank.end.BankBackEnd.services;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import ma.bank.end.BankBackEnd.entities.BankAccount;
 import ma.bank.end.BankBackEnd.entities.Customer;
+import ma.bank.end.BankBackEnd.repositories.AccountOperationRepo;
+import ma.bank.end.BankBackEnd.repositories.BankAccountRepo;
+import ma.bank.end.BankBackEnd.repositories.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
 public class ImpBankServiceAccount implements BankAccountService{
+
+    private BankAccountRepo bankAccountRepo;
+    private AccountOperationRepo accountOperationRepo;
+    private CustomerRepo customerRepo;
     @Override
     public Customer saveCustomer(Customer customer) {
-        return null;
+        log.info("Has been saved");
+        return customerRepo.save(customer);
     }
 
     @Override
